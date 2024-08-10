3ca3b98cc44048853b695d15a4cca33b8f751080
A_noob_in_coding <naeemabdulrehman77@gmail.com>
2024-08-10 23:34:46 +0500

    added animations for nav menu and increased gap in mobile layout

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ open_btn.style.display = "block"
 close_btn.style.display = "none"
 let nav = document.getElementById("nav")
 let menu_btn = document.getElementById("menu-btn");
-nav.classList.toggle("change")
 document.getElementById("menu-btn").addEventListener("click",()=>{
     if(open_btn.style.display == "block"){
         open_btn.style.display ="none"
         close_btn.style.display = "block"
-        nav.classList.toggle("change")
+        nav.style.width = "80vw";
+        console.log(nav.style.width)
     }
     else{
         open_btn.style.display = "block"
         close_btn.style.display = "none"    
-        nav.classList.toggle("change")
+        nav.style.width = 0;
+        console.log(nav.style.width)
     }
     
 })
